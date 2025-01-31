ef74f8a186810e9cd8f36e5be37ccc7f190eb35d
Ioan CHIRIAC <i.chiriac@yahoo.com>
2025-01-31 09:25:57 +0000

    init server

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.use(express.static('public'));
+app.use('/jquery/', express.static('node_modules/jquery/dist/'));
+app.use('/bootstrap/', express.static('node_modules/bootstrap/dist/'));
+
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
